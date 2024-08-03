b98dfbba1ded4d28cc80e04d848b0fd2856b6768
HafizIkhsan <hafizikhsan73@gmail.com>
2024-08-03 17:41:21 +0700

    fix profile dropdown

--- js/profile-dropdown.js
+++ js/profile-dropdown.js
@@ -1,8 +1,14 @@
 const profile  = document.querySelector('#profile');
+const button = profile.querySelector('button');
 
-profile.addEventListener('click',function(e){
-    const dropdown = e.target.nextElementSibling;
-    dropdown.classList.toggle('active');
+button.addEventListener('click',function(e){
+    const dropdown = profile.nextElementSibling;
+    if(dropdown){
+        profile.classList.toggle('active')
+        dropdown.classList.toggle('active');
+    }else{
+        console.error();
+    }
 });
 
 
