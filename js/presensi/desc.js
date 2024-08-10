d4b27c997c1be43276f6131f19cc3e9b409492c8
HafizIkhsan <hafizikhsan73@gmail.com>
2024-08-10 21:27:01 +0700

    overlay desc done

---
+++ js/presensi/desc.js
@@ -0,0 +1,9 @@
+const tuple = document.querySelector('.tuple');
+const kdMk = document.querySelector('.kd-mk').innerHTML;
+const mk = document.querySelector('.matkul').innerHTML;
+const kls = document.querySelector('.kls').innerHTML;
+const hari = document.querySelector('.hari').innerHTML;
+const jadwal = document.querySelector('.jadwal').innerHTML;
+const ruangan = document.querySelector('.ruangan').innerHTML;
+
+tuple.addEventListener('click')
--- js/presensi/popup.js
+++ js/presensi/popup.js
@@ -1,18 +1,39 @@
+// Pop-up
 const tuple = document.querySelectorAll('.tuple');
 const overlay = document.getElementById('pop-up');
 const close = document.getElementById('close');
 const background = document.getElementById('background-popup');
 
-tuple.forEach(function(tuple){
+//Desc-tuple
+const kdMk = document.getElementsByClassName('kd-mk');
+const mk = document.getElementsByClassName('matkul');
+const kls = document.getElementsByClassName('kls');
+const hari = document.getElementsByClassName('hari');
+const jadwal = document.getElementsByClassName('jadwal');
+const ruangan = document.getElementsByClassName('ruangan');
+
+
+tuple.forEach(function(tuple, index){
     tuple.addEventListener('click',function(){
+        const nmMk = mk[index];
+        const kd_mK = kdMk[index];
+        const clss = kls[index];
+        const day = hari[index];
+        const schedule = jadwal[index];
+        const room = ruangan[index];
+        document.getElementById('nm-matkul').innerHTML = nmMk.innerHTML;
+        document.getElementById('kd-mk').innerHTML = kd_mK.innerHTML +' & '+ clss.innerHTML;
+        document.getElementById('jdwl').innerHTML = day.innerHTML +', '+ schedule.innerHTML +' & '+ room.innerHTML;
         overlay.style.display = 'block';
         background.style.display = 'block';
+
     });
 });
 
 close.addEventListener('click',function(){
     overlay.style.display = 'none';       
     background.style.display = 'none';
+    document.getElementById('nm-matkul').innerHTML = '';
 });
 
 background.addEventListener('click',function(){
