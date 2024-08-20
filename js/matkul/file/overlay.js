8707db81fce716ca29cd0c2866d85d22690634b7
HafizIkhsan <hafizikhsan73@gmail.com>
2024-08-20 23:23:59 +0700

    finish full-screen overlay

--- js/matkul/file/overlay.js
+++ js/matkul/file/overlay.js
@@ -3,25 +3,52 @@ const materi = document.querySelector(".materi");
 const background = document.getElementById("background-popup");
 const overlayTugas = document.getElementById("overlay-tugas");
 const overlayMateri = document.getElementById("overlay-materi");
+const overlay = document.querySelector(".overlay");
+const close = document.querySelectorAll(".close");
 const body = document.body;
 
+//Shows overlay
 tugas.addEventListener("click", function () {
   background.style.display = "block";
   body.style.overflow = "hidden";
-  overlayTugas.style.display = "block";
+  overlay.style.display = "block";
 });
 
 materi.addEventListener("click", function () {
   background.style.display = "block";
   body.style.overflow = "hidden";
-  overlayMateri.style.display = "block";
+  overlay.style.display = "block";
+});
+
+//Close overlay
+close.forEach(function (e) {
+  e.addEventListener("click", function () {
+    background.style.display = "none";
+    body.style.overflow = "scroll";
+    overlay.style.display = "none";
+    overlay.classList.remove("ful");
+  });
 });
 
 background.addEventListener("click", function () {
   background.style.display = "none";
   body.style.overflow = "scroll";
-  overlayTugas.style.display = "none";
-  overlayMateri.style.display = "none";
+  overlay.style.display = "none";
+  overlay.classList.remove("ful");
 });
 
-export { tugas, materi, background };
+// Expand & Shrink Overlay
+const expand = document.querySelectorAll(".expand");
+expand.forEach(function (e) {
+  e.addEventListener("click", function () {
+    if (overlay.classList.contains("full")) {
+      overlay.classList.remove("full");
+      e.setAttribute("src", "../../assets/Icon/Light/Expand.svg");
+    } else {
+      overlay.classList.add("full");
+      e.setAttribute("src", "../../assets/Icon/Light/Shrink.svg");
+    }
+  });
+});
+
+export { tugas, materi, background, close };
--- js/matkul/file/script.js
+++ js/matkul/file/script.js
@@ -1,2 +1,2 @@
 import profile from "../../dashboard/profile-dropdown.js";
-import { tugas, materi, background } from "../file/overlay.js";
+import { tugas, materi, background, close } from "../file/overlay.js";
