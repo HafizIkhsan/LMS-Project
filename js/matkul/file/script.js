23091b1ad16f24791cfbfc46b5a70cc5369bf30d
HafizIkhsan <hafizikhsan73@gmail.com>
2024-08-25 12:04:46 +0700

    finish expand post in matkul page

--- js/matkul/file/script.js
+++ js/matkul/file/script.js
@@ -1,3 +1,3 @@
 import profile from "../../dashboard/profile-dropdown.js";
 import { tugas, materi, background, close } from "../file/overlay.js";
-import textarea from "./textarea.js";
+import { container, batalkanButton } from "./textarea.js";
--- js/matkul/file/textarea.js
+++ js/matkul/file/textarea.js
@@ -1,7 +1,40 @@
-const textMain = document.getElementById("main-input");
+const inputClick = document.querySelector(".click-expand");
+const container = document.querySelector(".input-container");
+const textArea = document.getElementById("main-input");
+const additionals = container.querySelectorAll(".additional");
+const batalkanButton = container.querySelector("#batalkan");
+const submitButton = container.querySelector("#submit");
 
-textMain.addEventListener("click", function () {
-  textMain.setAttribute("rows", "4");
+inputClick.addEventListener("click", function () {
+  inputClick.style.display = "none";
+  textArea.setAttribute("rows", "4");
+  textArea.style.marginBottom = "16px";
+  textArea.style.backgroundColor = "#f8f8f8";
+  for (let i = 0; i < additionals.length; i++) {
+    if (i == 0) {
+      additionals[i].style.display = "block";
+    } else {
+      additionals[i].style.display = "flex";
+    }
+  }
+
+  textArea.addEventListener("input", function (e) {
+    if (e.target.value.length !== 0) {
+      submitButton.classList.add("active");
+    } else {
+      submitButton.classList.remove("active");
+    }
+  });
 });
 
-export default textarea;
+batalkanButton.addEventListener("click", function () {
+  inputClick.style.display = "block";
+  textArea.setAttribute("rows", "1");
+  textArea.style.marginBottom = "0px";
+  textArea.style.backgroundColor = "white";
+  for (let i = 0; i < additionals.length; i++) {
+    additionals[i].style.display = "none";
+  }
+});
+
+export { container, batalkanButton };
