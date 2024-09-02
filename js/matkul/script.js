d7668a31ce0409ea6163baf61697c01ed08cc71c
HafizIkhsan <hafizikhsan73@gmail.com>
2024-09-02 21:22:55 +0700

    fix header all page & start fixing presensi mobile page

--- js/matkul/script.js
+++ js/matkul/script.js
@@ -1,2 +1,3 @@
 import profile from "../dashboard/profile-dropdown.js";
 import info from "./list-matkul.js";
+import hamburger from "../dashboard/sidePanelMobile.js";
--- js/presensi/script.js
+++ js/presensi/script.js
@@ -1,2 +1,4 @@
 import profile from "../dashboard/profile-dropdown.js";
 import popup from "./popup.js";
+import {close} from '../matkul/file/overlay.js'
+import hamburger from "../dashboard/sidePanelMobile.js";
